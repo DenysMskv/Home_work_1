b666ab61babde673d0cfd71f5f0ba339f9be3d14
Denys <moskin.msk@gmail.com>
2024-01-19 23:42:47 +0200

    work

---
+++ Home_work_2.py
@@ -0,0 +1,16 @@
+name = input("Please enter four digit number, from 0 to 9:")
+name_1 = "Hi, here is your result."
+
+digit_1 = int(name) // 1000
+digit_2 = int(name) // 100 % 10
+digit_3 = int(name) // 10 % 10
+digit_4 = int(name) % 10
+
+print(digit_1)
+print(digit_2)
+print(digit_3)
+print(digit_4)
+print(name_1)
+
+
+
