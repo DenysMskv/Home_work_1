3a7ecadfb3b86f2924d96248a38ab5052cd6b58c
Denys <moskin.msk@gmail.com>
2024-01-25 17:21:06 +0200

    work

--- Home_work_3.1.py
+++ Home_work_3.1.py
@@ -1,7 +1,7 @@
 my_list = [1, 2, 3, 6, 7]
 my_list_1 = [1, 2, 3, 6, 7]
 my_list_1.remove(7)
-my_list_1.insert(0,7)
+my_list_1.insert(0, 7)
 
 result = f"{my_list} \t -> \t {my_list_1}"
 print(result)
