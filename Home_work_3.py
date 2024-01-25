0a14e2de2126ffebac6df1690e66dc431a6c7c4c
Denys <moskin.msk@gmail.com>
2024-01-25 15:03:44 +0200

    work

--- Home_work_3.py
+++ Home_work_3.py
@@ -1,5 +1,17 @@
-number_1 = input("Select number:")
-number_2 = input("Select next number:")
-actions = input("Choose an action; +, -, *, /:")
+number_1 = float(input("Select number:"))
+number_2 = float(input("Select next number:"))
+action = input("Choose an action; +, -, *, /:")
 
-print
+
+if action == '+':
+    result = number_1 + number_2
+elif action == '-':
+    result = number_1 - number_2
+elif action == '*':
+    result = number_1 * number_2
+elif action == '/':
+    result = number_1 / number_2
+else:
+    result = "Invalid action selected"
+
+print("Result:", result)
